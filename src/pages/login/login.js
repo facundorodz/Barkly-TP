dc6c9ca73233f72ceba1490fc4af8d87eb90f9df
facundorodz <frodriguezs@fi.uba.ar>
2025-11-17 20:48:32 -0300

    Escribo la estructura inicial de la función para redigir dependiendo si es Usuario o Superheroe

---
+++ src/pages/login/login.js
@@ -0,0 +1,8 @@
+function redirigir() {
+    if (document.getElementById("btnradio1").checked) {
+        console.log('Elegiste Usuario');
+    } 
+    else {
+        console.log('Elegiste Superheroe');
+    }
+}
