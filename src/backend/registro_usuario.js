86fcd2fb3d5d582dc9df52a602af74fa588bb9e4
[estebanDTZ] <[epogonza@fi.uba.ar]>
2025-11-20 18:04:44 +0000

    Cree la coneccion del formulario html con la base de datos atreves del .js

---
+++ src/backend/registro_usuario.js
@@ -0,0 +1,39 @@
+const { Pool } = require("pg");
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(express.json()); 
+app.use(express.static("src/public"));
+
+const pool = new Pool({
+  user: "postgres",
+  host: "localhost",
+  database: "postgres",
+  password: "2024",
+  port: 5432
+});
+
+app.post("/registrar", async (req, res) => {
+  try {
+    const {
+      nombre_completo, nombre_perfil, contrasena, cantidad_perros
+    } = req.body;
+
+    const query = `
+      INSERT INTO usuarios (nombre_completo, nombre_perfil, contrasena, cantidad_perros)
+      VALUES ($1, $2, $3, $4)
+    `;
+
+    await pool.query(query, [nombre_completo, nombre_perfil, contrasena, cantidad_perros]);
+
+    res.json({ mensaje: "Usuario registrado correctamente" });
+  	} 
+  catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Error al registrar usuario" });
+  }
+});
+
+app.listen(3000, () => console.log("API funcionando en puerto 3000"));
