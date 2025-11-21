67afebe77c932fd085f80f470323b805d59d0b91
[estebanDTZ] <[epogonza@fi.uba.ar]>
2025-11-21 23:16:28 +0000

    Elimino la carpte de public

--- src/public/login/login.js
+++
@@ -1,8 +0,0 @@
-function redirigir() {
-    if (document.getElementById("btnradio1").checked) {
-        window.location.href = "../../index.html";
-    } 
-    else {
-        window.location.href = "index-superheroe.html";
-    }
-}
