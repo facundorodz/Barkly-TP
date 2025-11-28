6e87a5cc1049aa53b3cda30030491bd606e3596b
[estebanDTZ] <[epogonza@fi.uba.ar]>
2025-11-28 23:23:13 +0000

    El navegador lee la info de la bdd pero aun no se imprime en el post

--- src/backend/routes/cuidadores.routes.js
+++ src/backend/routes/cuidadores.routes.js
@@ -3,16 +3,13 @@ const router = express.Router();
 const controlador = require("../services/controller.cuidadores.js");
 
 // CRUD CUIDADORES
-//router.post("/cuidadores", controlador.crearCuidador);
 router.get("/cuidadores", controlador.obtenerCuidadores);
 router.get("/cuidadores/:id", controlador.obtenerCuidadorPorID);
 router.put("/cuidadores/:id", controlador.editarCuidador);
 router.delete("/cuidadores/:id", controlador.eliminarCuidador);
 
-// CRUD PAQUETES
-/*router.post("/cuidadores/:id/paquetes", controlador.registrarPaquete);
-router.put("/paquetes/:id_paquete", controlador.editarPaquete);
-router.delete("/paquetes/:id_paquete", controlador.eliminarPaquete);*/
+module.exports = router;
+
 
 //Verifica que el cuidador este registrado en la bdd
 //Devuelve true si existe la id, false en caso contrario
