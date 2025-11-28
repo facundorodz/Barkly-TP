c12561238100a4670326f9ca1da854487fb1bf11
[estebanDTZ] <[epogonza@fi.uba.ar]>
2025-11-28 23:23:13 +0000

    El navegador lee la info de la bdd pero aun no se imprime en el post

--- src/backend/server.js
+++ src/backend/server.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const usersRouter = require("./routes/users.js");
 const cuidadoresRouter = require("./routes/cuidadores.routes.js");
+const cors = require("cors");
 
 
 const app = express();
 const port = 3000;
 
+
+app.use(cors());
 app.use(express.urlencoded({ extended: true })); // para poder usar req.body
 app.use(express.json()); 
 app.use(express.static("src/public")); // nos muestra todo lo de la carpeta public desde ese punto en adelante -> sirve para poder cambiar de paginas por ejemplo
@@ -37,6 +40,10 @@ app.get("/cuidador-form", (req, res) => {
 app.use("/users", usersRouter);
 
 app.use("/", cuidadoresRouter);
+
+/*app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "/prueba_crud.html"));
+});*/
 
 
 app.listen(port, () => {
