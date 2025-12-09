48a7526a793aa795f8592e6d7b92fe3b35e1c7ef
Lon <dordonez@fi.uba.ar>
2025-12-09 19:20:04 +0000

    Se corrigen los campos de datos del usuario, se empieza la funcion de añadir mascota.

--- src/public/perfiles/perfil_usuario.js
+++ src/public/perfiles/perfil_usuario.js
@@ -1,3 +1,5 @@
+// Funcion cambiar foto
+
 function displayProfilePic() {
     const input = document.getElementById("photo");
     const img = document.getElementById ("profilePic");
@@ -14,8 +16,48 @@ function displayProfilePic() {
     }
 }
 
+//Funcion guardar datos
+
 function submitForm() {
     const formData = new FormData(document.getElementById("formPerfil"));
+
+    for (const pair of formData.entries()) {
+        console.log(pair[0] + ': ' + pair[1]);
+    }
+}
+
+//Añadir mascota pop-up
+
+const openModalButtons = document.querySelectorAll("[data-modal-target]")
+const closeModalButtons = document.querySelectorAll("[data-close-button]")
+const overlay = document.getElementById("overlay")
+
+openModalButtons.forEach(button => {
+    button.addEventListener("click", () => {
+        const modal = document.querySelector(button.dataset.modalTarget)
+        openModal(modal)
+    })
+})
+
+closeModalButtons.forEach(button => {
+    button.addEventListener("click", () => {
+        const modal = button.closest(".modal")
+        closeModal(modal)
+    })
+})
+
+function openModal(modal) {
+    if (modal == null) return
+    modal.classList.add("active")
+}
+
+function closeModal(modal) {
+    if (modal == null) return
+    modal.classList.remove("active")
+}
+
+function submitMascota() {
+    const formData = new FormData(document.getElementById("formMascota"));
 
     for (const pair of formData.entries()) {
         console.log(pair[0] + ': ' + pair[1]);
