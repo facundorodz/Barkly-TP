b9b16c1dd2b72be2a4ca37643ee30d4a9a7e566b
facundorodz <frodriguezs@fi.uba.ar>
2025-12-11 19:14:27 -0300

    Añado los chequeos particulares de cada campo

--- src/public/perfiles/perfil_cuidador.js
+++ src/public/perfiles/perfil_cuidador.js
@@ -1,17 +1,36 @@
-function submitForm() {
-    checkInputs()
-    // conexión a la BD.
+function agregarPaquetes() {
+    window.location.href = "../crear_post/crear_post.html"
 }
+
 
 function checkInputs() {
     const inputNombreCuidador = document.getElementById('nickname').value
     const inputFranquicia = document.getElementById('franquicia').value
     const inputPoderes = document.getElementById('poderes').value
+    const inputExperiencia = document.getElementById('experiencia').value
     const inputContrasena = document.getElementById('contrasena').value
     const inputPaquetesOfrecidos = document.getElementById('inputState').value
-
-    if (!inputNombreCuidador || !inputFranquicia || !inputPoderes || !inputContrasena || inputPaquetesOfrecidos == 'Elegir...') {
-        alert('Debe añadir TODOS los datos.')
-        return false;
+    
+    const CHEQUEO = true
+    // Chequeo general.
+    if (!inputNombreCuidador || 
+        !inputFranquicia || 
+        !inputPoderes || 
+        !inputExperiencia || 
+        !inputContrasena || 
+        inputPaquetesOfrecidos == 'Elegir...') {
+            alert('Debe añadir TODOS los datos.')
+            CHEQUEO = false
+        }
+        // Chequeos particulares.
+        else if (inputNombreCuidador.length > 120) { alert('El campo "nombre" no puede ser tan largo.'); CHEQUEO = false }
+        else if (inputFranquicia.length > 100) { alert('El campo "franquicia" no puede ser tan largo.'); CHEQUEO = false }
+        else if (inputExperiencia <= 0) { alert('El campo "experiencia" contiene un valor 0 o menor.'); CHEQUEO = false }
+        
+        return CHEQUEO 
     }
+
+function submitForm() {
+    checkInputs()
+    // conexión a la BD.
 }
