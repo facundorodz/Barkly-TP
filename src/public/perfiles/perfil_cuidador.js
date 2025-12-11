3be7bd0c0665a733ef36c92bb20d6153b6eb120f
facundorodz <frodriguezs@fi.uba.ar>
2025-12-11 18:25:04 -0300

    Añado una verificación a los inputs del form

--- src/public/perfiles/perfil_cuidador.js
+++ src/public/perfiles/perfil_cuidador.js
@@ -1,55 +1,17 @@
-
-function displayProfilePic() {
-    const input = document.getElementById("photo");
-    const file = input.files[0];
-
-    if (!file) return;
-
-    const reader = new FileReader();
-    reader.onload = function(e) {
-        const url = e.target.result;
-        document.getElementById("profilePic").src = url;
-
-        localStorage.setItem("profilePic", url);
-    };
-    reader.readAsDataURL(file);
-}
-////////////////////////////
-document.addEventListener("DOMContentLoaded", () => {
-    const savedImage = localStorage.getItem("profilePic");
-
-    if (savedImage) {
-        document.getElementById("profilePic").src = savedImage;
-    }
-});
-////////////////////////////
-
 function submitForm() {
-    const datos = {
-        nickname: document.getElementById("nickname").value,
-        franquicia: document.getElementById("franquicia").value,
-        password: document.getElementById("contrasena").value,
-        poderes: document.getElementById("poderes").value,
-        mascotas: JSON.parse(localStorage.getItem("mascotas")) || []
-    };
-
-    localStorage.setItem("perfilUsuario", JSON.stringify(datos));
-    alert("Datos guardados correctamente");
+    checkInputs()
+    // conexión a la BD.
 }
-////////////////////////////
-window.addEventListener("load", () => {
-    const datosGuardados = JSON.parse(localStorage.getItem("perfilUsuario"));
-    if (datosGuardados) {
-        document.getElementById("nickname").value = datosGuardados.nickname;
-        document.getElementById("franquicia").value = datosGuardados.franquicia;
-        document.getElementById("contrasena").value = datosGuardados.password;
-        document.getElementById("inputState").value = datosGuardados.perros;
-        mostrarPaquetes();
+
+function checkInputs() {
+    const inputNombreCuidador = document.getElementById('nickname').value
+    const inputFranquicia = document.getElementById('franquicia').value
+    const inputPoderes = document.getElementById('poderes').value
+    const inputContrasena = document.getElementById('contrasena').value
+    const inputPaquetesOfrecidos = document.getElementById('inputState').value
+
+    if (!inputNombreCuidador || !inputFranquicia || !inputPoderes || !inputContrasena || inputPaquetesOfrecidos == 'Elegir...') {
+        alert('Debe añadir TODOS los datos.')
+        return false;
     }
-});
-
-function mostrarPaquetes() {}
-
-function redirigir() {
-    window.location.href = "../crear_post/crear_post.html";
 }
