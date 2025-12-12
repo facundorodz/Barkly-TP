c4ef4ce6a7c786ae760d86e3a4c11397e34b55b4
[estebanDTZ] <[epogonza@fi.uba.ar]>
2025-12-12 16:13:20 +0000

    Traigo la carpeta del post de los perfiles para hacer la conexion con el back

---
+++ src/public/perfiles/perfil_cuidador.js
@@ -0,0 +1,43 @@
+function agregarPaquetes() {
+    window.location.href = "../crear_post/crear_post.html"
+}
+
+
+function checkInputs() {
+    const inputNombreCuidador = document.getElementById('nickname').value
+    const inputFranquicia = document.getElementById('franquicia').value
+    const inputPoderes = document.getElementById('poderes').value
+    const inputExperiencia = document.getElementById('experiencia').value
+    const inputContrasena = document.getElementById('contrasena').value
+    const inputPaquetesOfrecidos = document.getElementById('inputState').value
+    
+    const CHEQUEO = true
+    // Chequeo general.
+    if (!inputNombreCuidador || 
+        !inputFranquicia || 
+        !inputPoderes || 
+        !inputExperiencia || 
+        !inputContrasena || 
+        inputPaquetesOfrecidos == 'Elegir...') {
+            alert('Debe añadir TODOS los datos.')
+            CHEQUEO = false
+        }
+        // Chequeos particulares.
+        else if (inputNombreCuidador.length > 120) { alert('El campo "nombre" no puede ser tan largo.'); CHEQUEO = false }
+        else if (inputFranquicia.length > 100) { alert('El campo "franquicia" no puede ser tan largo.'); CHEQUEO = false }
+        else if (inputExperiencia <= 0) { alert('El campo "experiencia" contiene un valor 0 o menor.'); CHEQUEO = false }
+        
+        return CHEQUEO 
+    }
+
+// Chequeo para evitar que se pongan letras en el campo "experiencia"
+const experiencia = document.getElementById('experiencia')
+
+experiencia.addEventListener("input", () => {
+  experiencia.value = experiencia.value.replace(/[^0-9]/g, "")
+})
+
+function submitForm() {
+    checkInputs()
+    // conexión a la BD.
+}
