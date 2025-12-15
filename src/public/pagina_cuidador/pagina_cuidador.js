c688d2db3b67de9b734c31291acce7df8abcef4c
facundorodz <frodriguezs@fi.uba.ar>
2025-12-15 16:26:36 -0300

    Corrijo la conexión HTML con JS y añado la función para chequear los inputs

--- src/public/pagina_cuidador/pagina_cuidador.js
+++ src/public/pagina_cuidador/pagina_cuidador.js
@@ -17,3 +17,28 @@ document.getElementById("form-superheroe").addEventListener("submit", async (e) 
     const data = await res.json();
     alert(data.mensaje);
 });
+
+function checkInputs() {
+    const inputNombreCuidador = document.getElementById("input-nombre") 
+    const inputFranquicia = document.getElementById("input-franquicia")
+    const inputPoderes = document.getElementById("input-poderes")
+    const inputExperiencia = document.getElementById("input-experiencia")
+    
+    const CHEQUEO = true
+    if (!inputNombreCuidador || 
+        !inputFranquicia || 
+        !inputPoderes || 
+        !inputExperiencia ) { 
+            alert('Debe añadir TODOS los datos.')
+            CHEQUEO = false
+        }
+        // Chequeos particulares.
+        else if (inputNombreCuidador.length > 120) { alert('El campo "nombre" no puede ser tan largo.'); CHEQUEO = false }
+        else if (inputFranquicia.length > 100) { alert('El campo "franquicia" no puede ser tan largo.'); CHEQUEO = false }
+        
+        return CHEQUEO
+}
+
+function submitForm() {
+    checkInputs()
+}
