08ea039a0b4b50d4d0e4a675091cdeaccfcba30e
Ignacio Blanco <ignacioblanco06122005@gmail.com>
2026-02-20 16:44:48 -0300

    mas pruebas

--- src/backend/bdd/bdd.js
+++ src/backend/bdd/bdd.js
@@ -1,20 +1,17 @@
-
-const {Pool} = require("pg");
+const { Pool } = require("pg");
 
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT),
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 pool.connect()
-  .then(() => console.log(" Conectado a PostgreSQL"))
-  .catch(err => console.error(" Error al conectar a PostgreSQL:", err));
+  .then(() => console.log("Conectado a PostgreSQL"))
+  .catch(err => console.error("Error al conectar a PostgreSQL:", err));
 
 module.exports = pool;
-
 
 /* como levantar la BD -> 
 
